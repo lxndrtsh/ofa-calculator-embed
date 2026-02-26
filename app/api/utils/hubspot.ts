4ba8738e8a0cf254f97d54b62f1c3e7f6b98d1b7
Alexander Tash <lxndr.tsh@gmail.com>
2026-02-26 10:53:03 -0600

    quick cleanup

--- app/api/utils/hubspot.ts
+++ app/api/utils/hubspot.ts
@@ -61,7 +61,6 @@ export async function sendToHubSpot(data: HubSpotContactData): Promise<{ success
     if (data.company) contactProperties.calculator_input_company = data.company;
     if (data.title) contactProperties.calculator_input_jobtitle = data.title;
     if (data.state) contactProperties.calculator_input_state = data.state;
-    if (data.state) contactProperties.calculator_input_state_name = data.state;
     if (data.county) contactProperties.calculator_input_county = data.county;
 
     // Add form type
